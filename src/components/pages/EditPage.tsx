bd476752fe8c6967c48c322b381fcb6a91fbbf72
Elena Keda <elena_keda_92@mail.ru>
2021-05-24 14:09:05 +0300

    add btn "Back to main list"

--- src/components/pages/EditPage.tsx
+++ src/components/pages/EditPage.tsx
@@ -5,10 +5,12 @@ import { SomeTodo } from '../SomeTodo';
 import { useService } from '../Hooks';
 import { observer } from 'mobx-react';
 import { InfoCircleOutlined, ScheduleOutlined, UserOutlined } from '@ant-design/icons';
-import {Redirect} from 'react-router-dom';
+import {Redirect, useHistory} from 'react-router-dom';
 import { Button, notification, Space } from 'antd';
 
+
 export const EditPage = (props:any) => {
+  const history = useHistory();
 
   const openNotificationWithIcon = type => {
     notification[type]({
@@ -22,22 +24,25 @@ export const EditPage = (props:any) => {
   return(
     <>
       <h1>Edit task</h1>
-        <Input
-          onChange={()=>console.log(props.location)}
-          value={props.location.state.title}
-          placeholder="Edit your task"
-          prefix={<ScheduleOutlined className="site-form-item-icon" />}
-          suffix={
-            <Tooltip title={`id:${props.location.state.id}`}>
-              <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-            </Tooltip>
-          }
-        />
-        <br />
-        <br />
-        <Space>
-          <Button onClick={() => openNotificationWithIcon('info')}>Info about task</Button>
-        </Space>
+      <Button type="primary" onClick={() => history.push('/')}>Back to main list</Button>
+      <br />
+      <br />
+      <Input
+        onChange={()=>console.log(props.location)}
+        value={props.location.state.title}
+        placeholder="Edit your task"
+        prefix={<ScheduleOutlined className="site-form-item-icon" />}
+        suffix={
+          <Tooltip title={`id:${props.location.state.id}`}>
+            <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+          </Tooltip>
+        }
+      />
+      <br />
+      <br />
+      <Space>
+        <Button onClick={() => openNotificationWithIcon('info')}>Info about task</Button>
+      </Space>
     </>
   )
 }
